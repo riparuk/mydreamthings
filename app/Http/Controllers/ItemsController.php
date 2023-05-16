aecc8823c6359e30a4da3141317216880114cfa3
riparuk <faruqirifa@gmail.com>
2023-05-16 16:09:12 +0700

    send items of user to view

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Item;
 use App\View\Components\listitem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ItemsController extends Controller
 {
     //
     public function index(Item $items){
-        return view('home', 
-        [
-            "items" => $items::all()
-        ]);
+            $items = Auth::user()->items;
+            
+            return view('home', compact('items'));
     }
 }
